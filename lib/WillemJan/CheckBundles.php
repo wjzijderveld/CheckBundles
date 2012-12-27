a08c45115a5ac9b2a96418276ae1c3ffb8a6c5f8
Willem-Jan <willemjan@MacBook-Pro-van-Marko-Leer.local>
2012-12-27 20:53:28 +0100

    Initial version of composer.json and CheckBundles class

---
+++ lib/WillemJan/CheckBundles.php
@@ -0,0 +1,19 @@
+<?php
+namespace WillemJan;
+
+use Composer\Scripts\Event;
+
+/**
+ * Description of CheckBundles
+ *
+ * @author willemjan
+ */
+class CheckBundles {
+    
+    static public function postUpdate(Event $event) {
+        
+    }
+    
+}
+
+?>
